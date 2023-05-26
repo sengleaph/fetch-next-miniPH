454aa551af65e3fdf30cec8d99ae2fa47f264c99
Seng-Leaph <sengleaphseang@gmail.com>
2023-05-27 03:25:33 +0700

    products

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <>
+        <img style={{width:'100%', height:"100vh"}} src="https://i1.wp.com/hypebeast.com/wp-content/blogs.dir/6/files/2017/12/2018-christmas-memes-gifs-grinch-mean-girls-0.gif?w=960" alt="..."/>
+      </>
+    </div>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,34 @@
+import Card from "@/components/Card";
+export const metadata = {
+  title: "Home Page",
+  description: "This is the home page",
+};
+
+// get data from API
+export async function getData() {
+  // no-store to avoid cache
+  const res = await fetch(
+    "https://api.escuelajs.co/api/v1/products", {cache: "no-store"}
+  );
+  const data = await res.json();
+  return data;
+}
+
+export default async function Home() {
+  const products = await getData();
+
+  return (
+    <main className="flex  flex-wrap items-center justify-between p-24">
+      {products.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            image={product.images[0]}
+          />
+        ))}
+    </main>
+  );
+}
